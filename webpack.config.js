2428a4ec25fbcb8bda61d345d26a3bd1343c0950
Ludo <ludoloops@protonmail.com>
2020-06-19 16:34:26 +0200

    Feat: compression d'image

--- webpack.config.js
+++ webpack.config.js
@@ -17,18 +17,39 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(jpg|jp(e*)g|gif|png)$/,
+        test: /\.(gif|png|jpe?g)$/i,
         use: [
           {
             loader: 'url-loader',
             options: {
-              limit: 1020,
+              limit: 8192,
               name: '[name]-[contenthash].[ext]',
               outputPath: 'images',
             },
           },
+          // 'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+            },
+          },
         ],
-      }, // end images rule
+      },
       {
         test: /\.svg$/,
         use: [
